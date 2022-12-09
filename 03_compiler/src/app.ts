6744e9be5f3e503f9a8dcd6762f9f225e084b528
mishka88ru <b_mike@rambler.ru>
2022-12-09 23:39:57 +0300

    03 - compiler (end)

---
+++ 03_compiler/src/app.ts
@@ -0,0 +1,8 @@
+
+const userName = "Mike";
+console.log(`Time to get started, ${userName}...`);
+
+const button = document.querySelector("button")!;
+button.addEventListener("click", () => {
+	console.log("Clicked!");
+});
