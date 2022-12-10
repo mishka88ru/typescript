c3fb074ff8fa29089572e6f711bffceb00ecd08f
mishka88ru <b_mike@rambler.ru>
2022-12-10 13:15:31 +0300

    04 - next gen js (done)

--- 04_next_gen/src/app.ts
+++ 04_next_gen/src/app.ts
@@ -21,13 +21,13 @@ function add(a: number, b: number) {
 
 add(10, 20);
 
-const add2 = (a: number, b: number) => {
+const add2 = (a: number, b: number = 1) => {
 	return a + b;
 }
 
 const add3 = (a: number, b: number) => a + b;
 
-console.log(add2(3, 4));
+console.log(add2(3));
 console.log(add3(3, 4));
 
 type PRINTER = (output: string | number) => void;
@@ -39,3 +39,46 @@ const button = document.querySelector("button");
 if (button) {
 	button.addEventListener("click", event => console.log("Clicked", event));
 }
+
+
+const hobbies = [ "Sports", "Cooking" ];
+const activeHobbies = [ "Hiking" ]
+
+//activeHobbies.push(hobbies[0], hobbies[1]);
+activeHobbies.push(...hobbies);
+console.log(activeHobbies);
+console.log(...activeHobbies);
+
+
+const person = {
+	name: "Mike",
+	age: 33
+};
+const copiedPerson = person; // ref
+copiedPerson.age++;
+console.log(person);
+
+const realCopy = { ...person };
+realCopy.name = "Mickey";
+realCopy.age++;
+console.log(realCopy, person);
+
+
+const mul = (...nums: number[]) => {
+	let res = nums.reduce((cur, val) => {
+		return cur * val;
+	}, 1);
+	return res;
+};
+const multiply = mul(1, 2, 3, 4, 5);
+console.log(multiply);
+
+
+//const hobby1 = hobbies[0];
+//const hobby2 = hobbies[1];
+const [hobbyA, hobbyB, ...otherHobbies] = activeHobbies;
+console.log(hobbyA, hobbyB, otherHobbies);
+
+
+const { name: myName, age: myAge } = person;
+console.log(myName, myAge);
